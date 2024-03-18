60ec57e1dc141e64bb279e085745fb4ef91bd2b9
Mael <163682734+Mayel-0@users.noreply.github.com>
2024-03-18 19:41:56 +0100

    amélioration du code et ajout du nom sur l'image

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import os
 import face_recognition
 import cv2
 
-
 name_file = input("tapez le nom de votre image a traiter !")
-print("traitement en cours...")
 name_file_test = name_file + "Vtest" + ".jpg"
-name_file = name_file + ".jpg"
+name_file_img = name_file
+name_file = "image_test/" + name_file + ".jpg"
 
 IMAGES_DIR = os.path.join(os.getcwd(), 'image_reconnaissable')
 
@@ -26,11 +25,18 @@ def get_face(image):
 def draw_rectangle(image, output, coordinates):
     image =  cv2.imread(image)
     x, y, w, h = coordinates
-    cv2.rectangle(image, (x, y), (w, h), (0, 0, 255), 10)
+    cv2.rectangle(image, (x, y), (w, h), (0, 0, 255), 15)
+    cv2.putText(image, os.path.basename(name_file_img), (int((x+h)/2),y-30), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 10), cv2.FONT_HERSHEY_SIMPLEX
     cv2.imwrite(os.path.join(os.getcwd(), "output", output) ,image)
+
+
+
+print("traitement en cours...")
 
 print(get_face(name_file))
 
 draw_rectangle(name_file, name_file_test, get_face(name_file)[0])
+
+print("traitement termine !")
 
 
