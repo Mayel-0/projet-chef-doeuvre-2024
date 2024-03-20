6fdb527d4b0a0e5fb9b428f5926fe356bdc0936c
Mael <163682734+Mayel-0@users.noreply.github.com>
2024-03-20 20:44:27 +0100

    reconnaitre les visage et a qui elle corresponde

--- main.py
+++ main.py
@@ -3,27 +3,56 @@ import face_recognition
 import cv2
 import numpy as np
 
-def get_difference_images(image1, image2):
-    image1 = face_recognition.load_image_file(image1)
-    image2 = face_recognition.load_image_file(image2)
-    image1 = np.array(image1)
-    image2 = np.array(image2)
-    difference_images = np.subtract(image1, image2)
-    return difference_images
 
+mike_tyson_img = face_recognition.load_image_file("image_test/mike tyson.jpg")
+mike_tyson_face_encoding = face_recognition.face_encodings(mike_tyson_img)[0]
+
+brad_pitt_img = face_recognition.load_image_file("image_test/brad pitt.jpg")
+brad_pitt_face_encoding = face_recognition.face_encodings(brad_pitt_img)[0]
+
+lewis_hamilton_img = face_recognition.load_image_file("image_test/lewis hamilton.jpg")
+lewis_hamilton_face_encoding = face_recognition.face_encodings(lewis_hamilton_img)[0]
+
+squeezie_img = face_recognition.load_image_file("image_test/squeezie.jpg")
+squeezie_face_encoding = face_recognition.face_encodings(squeezie_img)[0]
+
+baki_img = face_recognition.load_image_file("image_test/baki.jpg")
+baki_face_encoding = face_recognition.face_encodings(baki_img)[0]
+
+adriana_lima_img = face_recognition.load_image_file("image_test/adriana lima.jpg")
+adriana_lima_face_encoding = face_recognition.face_encodings(adriana_lima_img)[0]
+
+known_face_encodings = [
+    mike_tyson_face_encoding,
+    brad_pitt_face_encoding,
+    lewis_hamilton_face_encoding,
+    squeezie_face_encoding,
+    baki_face_encoding,
+    adriana_lima_face_encoding
+    
+]
+
+known_face_names = [
+    "Mike Tyson",
+    "Brad Pitt",
+    "Lewis Hamilton",
+    "Squeezie",
+    "Baki",
+    "Adriana Lima"
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
 
 name_file = input("tapez le nom de votre image a traiter !")
 name_file_test = name_file + "Vtest" + ".jpg"
 name_file_img = name_file
 name_file = "image_test/" + name_file + ".jpg"
 
-user_input = int(input("voulez vous faire une difference image ? si oui tapez 1 sinon 0 :"))
-if user_input == 1:
-    name_file_diff = input("avec quel image souhaiter vous faire une difference ?")
-    name_file_diff = "image_test/" + name_file_diff + ".jpg"
+unknown_image = face_recognition.load_image_file(name_file)
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-elif user_input != 0 and user_input != 1:
-    pass
 
 # Get all images in a directory
 def get_images(path):
@@ -38,31 +67,27 @@ def get_face(image):
     face_locations = face_recognition.face_locations(image)
     return face_locations[0]
 
+
 def draw_rectangle(image, output, coordinates):
     image =  cv2.imread(image)
     x, y, w, h = coordinates
     cv2.rectangle(image, (x, y), (w, h), (0, 0, 255), 10)
-    cv2.putText(image, os.path.basename(name_file_img), (int((x+h)/2),y-30), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 10)
+    cv2.rectangle(image, (x, y - 100), (w, y), (0, 0, 255), cv2.FILLED)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    cv2.putText(image, name_file_img, (x + 6, y - 6), font, 3.5, (255, 255, 255), 3)
     cv2.imwrite(os.path.join(os.getcwd(), "output", output) ,image)
 
-
+results = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
 
 print("traitement en cours...")
 
 print((name_file))
-print(get_face(name_file))
 
-if user_input == 1: print((name_file_diff) + get_face(name_file_diff))
-elif user_input != 0 and user_input != 1:
-    pass
+print(get_face(name_file))
 
 draw_rectangle(name_file, name_file_test, get_face(name_file))
 
-if user_input == 1:
-    difference_images = get_difference_images(name_file, name_file_diff)
-    cv2.imwrite(os.path.join(os.getcwd(), "image_difference", "difference_images.jpg"), difference_images)
-elif user_input != 0 and user_input != 1:
-    pass
+print(results)
 
 print("traitement termine !")
 
