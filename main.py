2b84175673fd938cba8f7aeacb3030ae0a1f7e31
Mael <163682734+Mayel-0@users.noreply.github.com>
2024-03-28 16:37:06 +0100

    live webcam face recognition

--- main.py
+++ main.py
@@ -15,6 +15,57 @@ name_file = os.path.join(os.getcwd(), "image_test", f"{name_file}.jpeg")
 unknown_image = face_recognition.load_image_file(name_file)
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
+#Live webcam recognition
+def webcam_recognition():
+    video_capture = cv2.VideoCapture(0)
+
+    mael_llado_image = face_recognition.load_image_file("now_people_face/mael.jpeg")
+    mael_llado_face_encoding = face_recognition.face_encodings(mael_llado_image)[0]
+
+    sulyvan_rouzeau_image = face_recognition.load_image_file("now_people_face/sulyvan.jpeg")
+    sulyvan_rouzeau_face_encoding = face_recognition.face_encodings(sulyvan_rouzeau_image)[0]
+
+    known_face_encodings = [
+        mael_llado_face_encoding,
+        sulyvan_rouzeau_face_encoding
+    ]
+
+    known_face_names = [
+        "Mael llado",
+        "sulyvan rouzeau"
+    ]
+
+    while True:
+        ret, frame = video_capture.read()
+
+        rgb_frame = frame[:, :, ::-1]
+
+        face_locations = face_recognition.face_locations(frame)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 # Get all images in a directory
 def get_images(path):
@@ -38,6 +89,8 @@ def draw_rectangle(image, output, coordinates):
     font = cv2.FONT_HERSHEY_DUPLEX
     cv2.putText(image, name_file_img, (x + 6, y - 6), font, 3.5, (255, 255, 255), 3)
     cv2.imwrite(os.path.join(os.getcwd(), "output", output) ,image)
+
+webcam_recognition()
 
 known_people = get_images(os.path.join(os.getcwd(),"now_people_face"))
 known_face_encodings = []
